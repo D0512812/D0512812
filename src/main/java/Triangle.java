bebd2e9d0d4d6c9bcecbcf5f8c95bae4c8b4bcaa
D0512812 <d0512812@gmail.com>
2020-06-07 15:08:36 +0900

    fix java files

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,5 +1,4 @@
-package triangle;
-
+package HW;
 
 public class Triangle{ 
 
--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -1,14 +1,58 @@
-package triangle;
+package HW;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-public class TriangleTest { 
-	
+public class TriangleTest {
+
 	@Test
 	public void testCheckTriangle3() {
 		Triangle a = new Triangle();
 		assertEquals("1", a.CheckTriangle(2, 3, 4));
 	}	
+
+	@Test
+	public void testCheckTriangle2() {
+		Triangle a = new Triangle();
+		assertEquals("1", a.CheckTriangle(1, 1, 1));
+	}	
+		
+	
+	@Test
+	public void testCheckTriangle5() {
+		Triangle a = new Triangle();
+		
+		assertEquals("1", a.CheckTriangle(1, 2, 2));
+	}	
+		
+	@Test
+	public void testCheckTriangle6() {
+		Triangle a = new Triangle();
+		assertEquals("1", a.CheckTriangle(3, 4, 5));
+	}	
+
+	@Test
+	public void testCheckTriangle7() {
+		Triangle a = new Triangle();
+	
+		assertEquals("1", a.CheckTriangle(2, 3, 4));		
+	} 
+	
+	@Test
+	public void testCheckTriangle4() {
+		Triangle a = new Triangle();
+		
+		assertEquals("1", a.CheckTriangle(1, 1, 1.414));
+
+	}
+
+	@Test
+	public void testCheckTriangle1() {
+		Triangle a = new Triangle();
+		assertEquals("1", a.CheckTriangle(0, 0, 0));
+		assertEquals("1", a.CheckTriangle(-1, 2, 3));
+		assertEquals("1", a.CheckTriangle(2, -1, 5));		
+	}
+
 }
